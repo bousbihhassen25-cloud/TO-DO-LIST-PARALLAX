7c6c78a839d0d665bde664920e8c90782583047d
bousbihhassen25-cloud <bousbih.hassen25@gmail.com>
2025-09-11 00:27:49 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,44 +1,6 @@
-const CACHE_NAME = 'todo-parallax-v3-2';
-const ASSETS = [
-  '/TO-DO-LIST-PARALLAX/',
-  '/TO-DO-LIST-PARALLAX/index.html',
-  '/TO-DO-LIST-PARALLAX/manifest.webmanifest?v=3.2',
-  '/TO-DO-LIST-PARALLAX/icons/icon-192.png',
-  '/TO-DO-LIST-PARALLAX/icons/icon-512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    await cache.addAll(ASSETS);
-    self.skipWaiting();
-  })());
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-    const keys = await caches.keys();
-    await Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
-    self.clients.claim();
-  })());
-});
-
-self.addEventListener('fetch', (event) => {
-  const req = event.request;
-  if (req.method !== 'GET' || new URL(req.url).origin !== self.location.origin) return;
-  event.respondWith((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    const cached = await cache.match(req);
-    if (cached) return cached;
-    try {
-      const fresh = await fetch(req);
-      if (fresh.ok && req.url.startsWith(self.location.origin)) {
-        cache.put(req, fresh.clone());
-      }
-      return fresh;
-    } catch (err) {
-      if (req.mode === 'navigate') return cache.match('/TO-DO-LIST-PARALLAX/index.html');
-      throw err;
-    }
-  })());
-});
+const CACHE='todo-parallax-v3-2';
+const ASSETS=['/TO-DO-LIST-PARALLAX/','/TO-DO-LIST-PARALLAX/index.html','/TO-DO-LIST-PARALLAX/manifest.webmanifest?v=3.2','/TO-DO-LIST-PARALLAX/icons/icon-192.png','/TO-DO-LIST-PARALLAX/icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{const u=new URL(e.request.url); if(e.request.method!=='GET'||u.origin!==self.location.origin)return;
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>(res.ok&&caches.open(CACHE).then(c=>c.put(e.request,res.clone())),res)).catch(()=>u.pathname==='/'||e.request.mode==='navigate'?caches.match('/TO-DO-LIST-PARALLAX/index.html'):Promise.reject())))});
